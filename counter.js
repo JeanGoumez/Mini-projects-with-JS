de2fba452fd0dea4334d7c30d83c9566b8f72d98
Jean Goumez <adrielgo.mez123@gmail.com>
2023-09-28 14:17:40 -0500

    first commit

---
+++ counter.js
@@ -0,0 +1,9 @@
+export function setupCounter(element, operacion) {
+  let counter = 0;
+  const setCounter = (count) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(counter + operacion));
+  setCounter(0);
+}
